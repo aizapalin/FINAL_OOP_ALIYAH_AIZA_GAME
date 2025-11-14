ae60e00b559a9d7c7af35a79be4e9ca1c0e84bb4
aizapalin <aiza.palin@jmc.edu.ph>
2025-11-14 21:54:19 +0800

    Create Game.java as main controller for game

---
+++ Game.java
@@ -0,0 +1,7 @@
+// Game.java — Main controller
+public class Game {
+    public static void main(String[] args) {
+        RockPaperScissors game = new RockPaperScissors();
+        game.startGame(); // main game loop
+    }
+}
